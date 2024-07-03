bc9f6f1743fda18916d13be8262c2ad83e035b74
Mariusz Jaguścik <jaguscikm@gmail.com>
2024-07-03 13:20:28 +0200

    Dodanie aplikacji VariablesNull
    Dodanie aplikacji z przykładami null

---
+++ VariablesNull/Program.cs
@@ -0,0 +1,8 @@
+﻿
+// Null to brak wartości zmiennej. Każda zmienna zadeklarowana, której nie przypisano ŻADNEJ wartości ma wartość null
+
+// Aby zainicjować zmienną większości typów nullem należy dodać do nazwy typu zmiennej znak ? - który pozwala na przyjmowanie wartości null
+int? wiek = null;
+bool? urodziny = null;
+double? sredniaWieku = null;
+string pierwszeImie = null;
