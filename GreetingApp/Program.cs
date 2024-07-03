5870ae20ab1493a26a26ed07aa720051aefee27b
Mariusz Jaguścik <jaguscikm@gmail.com>
2024-07-03 09:56:05 +0200

    Dodanie drugiej prostej aplikacji
    Dodanie drugiej aplikacjia GreetingApp

---
+++ GreetingApp/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// 1. Powitanie użytkownika
+Console.WriteLine("Dzień dobry! Witaj w aplikacji pozdrawiającej");
+Console.WriteLine("Ta aplikacja została stworzona prze Mariusza Jaguścika");
+Console.WriteLine("------------------------------------------------------");
+Console.WriteLine();
+
+// 2. Zapytanie o imię
+Console.Write("Jak masz na imię? ");
+string? pierwszeImie = "";
+pierwszeImie = Console.ReadLine();
+Console.WriteLine();
+
+// 3. Pozdrowienie użytkownika po imieniu
+Console.WriteLine($"Cześć " + pierwszeImie);
+
+Console.WriteLine("Dziękuję za skorzystanie z mojej aplikacji!");
+Console.ReadLine();
