18348541b2a692544d7f2c9d75d6b776acde73c2
Mariusz Jaguścik <jaguscikm@gmail.com>
2024-07-03 13:00:41 +0200

    Dodanie aplikacji VariablesBoolean
    Dodanie prostej aplikacji z przykładami użycia zmiennej typu boolean

---
+++ VariablesBoolean/Program.cs
@@ -0,0 +1,9 @@
+﻿
+// Zmienne typu Boolean przyjmuje zawsze wartość true albo false
+bool czyKompletny = false;
+
+// Do inicjalizowania lub zmiany wartości zmienny typu boolean można używać negacji:
+czyKompletny = !czyKompletny;
+
+Console.WriteLine(czyKompletny);
+Console.WriteLine(!czyKompletny);
