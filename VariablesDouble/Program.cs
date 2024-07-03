31f990e246bd828f2697b8e65c185cda32e6c1dd
Mariusz Jaguścik <jaguscikm@gmail.com>
2024-07-03 12:36:07 +0200

    Dodanie aplikacji VarablesDouble
    Dodanie prostej aplikacji podającej przykłady użyciu zmiennej typu double

---
+++ VariablesDouble/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// Zmienna typu double zawiera liczby z miejscami po przecinku np.: 1,23.
+
+
+// Inicjalizacja zmiennej typu double
+double sredniaWieku;
+
+// Aby zapisywać liczby dziseiętne należy używać kropki do rozdzielenia liczby o miejsc po przecinku
+sredniaWieku = (42.0 + 23 + 62) / 3;
+
+Console.WriteLine(sredniaWieku);
+
+// Zmienne typu double są najczęściej wykorzystywane jeśli potrzebne są zmienne do wykonywania obliczeń matematycznych.
+// Biblioteka Math opiera się na wykorzystaniu zmiennych typu double.
