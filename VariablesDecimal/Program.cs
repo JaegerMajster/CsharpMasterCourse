1af13589910981d060eea0e627df228787642269
Mariusz Jaguścik <jaguscikm@gmail.com>
2024-07-03 12:51:17 +0200

    Dodanie aplikacji VariablesDecimal
    Dodanie prostej aplikacji pokazującej przykłady użycia zmiennej typu decimal

---
+++ VariablesDecimal/Program.cs
@@ -0,0 +1,12 @@
+﻿
+// Zmienne typu decimal utrzymują podobnie jak zmienne typu decimal liczby rzeczywiste.
+// Zmienne typu decimal posiadają większą precyzję
+// Zajmują jednak znacznie więcej miejsca w pamięci
+// Zmiennych typu decimal używa się praktycznie wyłącznie do pieniędzy lub wartości astronomicznych.
+
+decimal bilansKontaBankowego;
+
+// Przy inicjalizowaniu zmiennej typu decimal należy uważać, aby nie podawać wartości która może być zinterpretowana jako double.
+bilansKontaBankowego = 2.34M;
+
+Console.WriteLine(bilansKontaBankowego);
