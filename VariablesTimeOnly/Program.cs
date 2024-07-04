1b65e2e46a4dae3cc8f2b084e7aaad067e8607dc
Mariusz Jaguścik <jaguscikm@gmail.com>
2024-07-04 11:21:35 +0200

    Dodanie aplikacji VariablesTimeOnly
    Dodanie prostej aplikacji pokazującej użycie zmiennej typu TimeOnly

---
+++ VariablesTimeOnly/Program.cs
@@ -0,0 +1,7 @@
+﻿
+// Podobnie jak DateOnly tak TimeOnly przechowuje jedynie wskazaną godzinę
+DateTime dzien = DateTime.Parse(s: "12/05/1998 14:00");
+TimeOnly godzina = TimeOnly.FromDateTime(dzien);
+
+Console.WriteLine(dzien);
+Console.WriteLine(godzina);
