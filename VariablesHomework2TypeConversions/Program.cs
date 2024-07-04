80552fc20c53f43313d6329f339393f7848b54ba
Mariusz Jaguścik <jaguscikm@gmail.com>
2024-07-04 11:55:45 +0200

    Wykonanie aplikacji z zadania domowego nr 2
    Wykonanie prostej aplikacji z zadanie domowego dotyczącego konwersji typów zmiennych

---
+++ VariablesHomework2TypeConversions/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// 1.Przywitanie
+Console.WriteLine("Witaj w moim programie dotyczącym pracy domowej nr 2");
+
+// 2.Zapytanie o wiek użytkownika
+Console.Write("Podaj proszę swój aktualny wiek: ");
+int aktualnyWiek = int.Parse(Console.ReadLine());
+Console.WriteLine("-------------------------------------");
+
+// 3. Obliczenie wieku za 25 lat
+int wiekPozniej = aktualnyWiek + 25;
+
+// 4. Obliczenie wieku sprzed 25 lat
+int wiekWczesniej = aktualnyWiek - 25;
+
+// 5. Wypisanie wszystkich danych w konsoli
+Console.WriteLine($"Twój aktualny wiek to: {aktualnyWiek} lat");
+Console.WriteLine($"Za 25 lat będziesz miał: {wiekPozniej} lat");
+Console.WriteLine($"25 lat temu miałeś/aś: {wiekWczesniej} lat");
