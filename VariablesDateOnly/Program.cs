6cce88fb530ea4253613ddc5ef200e9f22533085
Mariusz Jaguścik <jaguscikm@gmail.com>
2024-07-04 11:15:16 +0200

    Dodanie aplikacji VariablesDateOnly
    Dodanie prostej aplikacji pokazującej użycie zmiennej typu DateOnly

---
+++ VariablesDateOnly/Program.cs
@@ -0,0 +1,7 @@
+﻿
+// Od .Net 6.0 wprowadzono nowy typ zmiennych zawierający jedynie samą datę
+DateOnly jakasData = DateOnly.Parse(s: "5/5/1995");
+DateTime jakasData2 = DateTime.Parse(s: "5/5/1995");
+
+Console.WriteLine($"Sama data: {jakasData}");
+Console.WriteLine($"Sama data z czasem: {jakasData2}");
