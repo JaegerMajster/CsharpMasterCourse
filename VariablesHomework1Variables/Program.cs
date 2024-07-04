aad986983bed92ffb15254e0905d24006482d9bc
Mariusz Jaguścik <jaguscikm@gmail.com>
2024-07-04 10:31:13 +0200

    Wykonanie aplikacji z zadania domowego
    Wykonanie aplikacji z zadania domowego dotyczącego prostych typów zmiennych.

---
+++ VariablesHomework1Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿
+string pierwszeImie = "Mariusz";
+int wiek = 44;
+bool czyZyje = true;
+string nrTelefonu = "531-279-563";
+
+Console.WriteLine("Witaj w moim programie. Oto kilka informacji o Tobie:");
+Console.WriteLine("");
+Console.WriteLine($"Masz na imię: {pierwszeImie}");
+Console.WriteLine($"Twój wiek to: {wiek}");
+Console.WriteLine($"Czy jesteś żywy: {czyZyje}");
+Console.WriteLine($"Twój nr telefonu: {nrTelefonu}");
+Console.ReadLine();
