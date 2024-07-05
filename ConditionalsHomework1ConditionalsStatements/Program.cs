45c46b0b676499266396c630e59d2a36eab2c4ed
Mariusz Jaguścik <jaguscikm@gmail.com>
2024-07-05 09:25:12 +0200

    Wykonanie aplikacji z zadania domowego nr 3
    Wykonanie aplikacji do zadania domowego dotyczącego instrukcji warunkowych

---
+++ ConditionalsHomework1ConditionalsStatements/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.WriteLine("Witaj w mojej aplikacji do pracy domowej z intrukcji warunkowych");
+//1.Zapytać się imię użytkownika
+Console.Write("Podaj prosze swoje imię: ");
+string? pierwszeimie  = Console.ReadLine();
+Console.WriteLine("--------------------------------");
+
+//2. Wykorzystać podane imię użytkownika i w zależności od tego czy podał imię Tim, Timothy, czy inne odpowiednioe powitać:
+Console.WriteLine("Z wykorzystaniem instrukcji warunkowej if:");
+if (pierwszeimie.ToLower() == "tim" || pierwszeimie.ToLower() == "timothy")
+//-Tim lub Timothy - "Witaj profesorku"
+{
+    Console.WriteLine("Witaj profesorku!");
+}
+//- inne - "Witaj młody padawanie"
+else
+{
+    Console.WriteLine("Witaj młody padawanie!");
+}
+Console.WriteLine("--------------------------------");
+
+//3. Do powyższego zadania wykorzystać również instrukcję warunkową switch
+Console.WriteLine("Z wykorzystaniem instrukcji warunkowej switch:");
+switch (pierwszeimie.ToLower())
+{
+    case "tim" or "timothy":
+        Console.WriteLine("Witaj profesorku!");
+        break;
+    default:
+        Console.WriteLine("Witaj młody padawanie!");
+        break;
+}
