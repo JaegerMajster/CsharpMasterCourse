e70bfff9c13566c28a59e0570c14d82381a495d7
Mariusz Jaguścik <jaguscikm@gmail.com>
2024-07-10 10:04:54 +0200

    Wykonanie zadania domowego nr 9
    Wykonanie zadania domowego dotyczącego użycia pętli foreach

---
+++ LoopsHomework3ForEach/Program.cs
@@ -0,0 +1,29 @@
+﻿
+string imieTekst;
+List<string> imiona = new();
+
+//1. Powitanie
+Console.WriteLine("Witaj w mojej aplikacji domowej dotyczącej użcia pętli foreach");
+Console.WriteLine("--------------------------------------------------------------");
+
+//2. Pytanie o imię w pętli (pozostaw puste aby zakończyć)
+do
+{
+    Console.Write("Proszę podaj swoje imię (aby nie dodawać imienia i zakończyć dodawanie nowych imion kliknij Enter): ");
+    imieTekst = Console.ReadLine();
+    if (imieTekst != "")
+    {
+        imiona.Add(imieTekst);
+    }
+} while (imieTekst != "");
+
+//3. Korzystając z pętli foreach przywitaj wszystkie wpisane osoby
+Console.Clear();
+foreach (var imie in imiona)
+{
+    Console.WriteLine($"Cześć {imie}!");
+}
+
+//4. Podziękowanie
+Console.WriteLine();
+Console.WriteLine("Dziękuję za skorzystanie  z mojej aplikacji!");
