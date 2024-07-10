ffbef1aae841c3c24c3bebea1aa48046c264db2e
Mariusz Jaguścik <jaguscikm@gmail.com>
2024-07-10 08:08:27 +0200

    Wykonanie zadania domowego nr 8
    Wykonanie zadania domowego dotyczącego użycia pętli for

---
+++ LoopsHomework2For/Program.cs
@@ -0,0 +1,25 @@
+﻿
+string dane;
+List<string> imiona = new();
+
+//1.Powitanie
+Console.WriteLine("Witaj w mojej domowej aplikacji dotyczącej pętli for");
+Console.WriteLine("----------------------------------------------------");
+
+//2.Prośba o wpisanie imion oddzielonych przecinkami bez spacji
+Console.Write("Wpisz proszę kilka imion rodzielonych przecinkami. Proszę nie stosuj spacji: ");
+dane = Console.ReadLine();
+
+//3. Podzielenie stringa na tablicę
+imiona = dane.Split(',').ToList();
+
+//4. Wypisanie pozdrowienia dla każdej z osób zapisanych w tablicy
+Console.Clear();
+for (int i = 0; i < imiona.Count; i++)
+{
+    Console.WriteLine($"Cześć {imiona[i]}!");
+}
+
+//5. Podziękowanie
+Console.WriteLine();
+Console.WriteLine("Dziękuję za skorzystanie z mojej aplikacji!");
