ac1df68058799e90c81ecc524d1ccb7c62d25d18
Mariusz Jaguścik <jaguscikm@gmail.com>
2024-07-12 13:15:29 +0200

    Metody część 1.
    Pierwsza część nauki na temat metod w C#

---
+++ Methods/KomendyKonsoli.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public static class KomendyKonsoli
+    {
+        // Deklaracja metody składa się z kilku elementów
+        // public - to określenie dostępu do klasy
+        // static - pozwala uruchamiać metody bezpośrednio w konsoli
+        // void - ten parametr decyduje, cz metoda ma zwracać jakieś czy nie (void oznacza, brak zwracania jakichkolwiek danych)
+        // PowiedzCzesc - nazwa metody - wykorzytywana do odwołania się do metody. Nazwa powinna być samowyjaśnialna, czyli jasno wskazywać co dana metoda robi
+        // () - parametry metody, najczęściej dane przekazywane z zewnątrz do środka metody
+        // {} - wewnątrz nawiasów znajdują się wszystkie instrukcje, które mają zostać wykonane po odwołaniu się do danej metody
+
+        // Do metody można dodawać dowolne parametry, które przekazywane są z zewnątrz. Należy je zadeklarować przy tworzeniu metody, wraz z typem danych.
+        public static void PowiedzCzesc(string pierwszeImie)
+        {
+            Console.WriteLine($"Siema {pierwszeImie}!");
+            Console.WriteLine("Miło Cię poznać!");
+            Console.WriteLine("-----------------------");
+        }
+        public static void Zegnaj(string pierwszeImie)
+        {
+            Console.WriteLine("-----------------------");
+            Console.WriteLine($"Żegnaj {pierwszeImie}.");
+            Console.WriteLine("Miło było Cię znać,");
+            Console.WriteLine("A teraz won!");
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// Metody to zbiory operacji które można wykonać odwołując się do nazwy metody
+// Metody powinno się tworzyć w oddzielnym pliku z klasą (patrz plik z klasą KomendyKonsoli)
+// W głównym programie powinno się jedynie wywoływać klasy, tak aby nie zaciemniać jego treści
+
+// Aby móc skorzystać z metod zapisanych w danej bibliotece, bez podawania pełnej ścieżki przy wywołaniu metody trzeba na początku pliku
+// zadeklarować użycie tej biblioteki. Wtedy można wywołać metodę jedynie przez powołanie się na nazwę klasy i odpowiednią zapisaną
+// w niej metodę. Patrz niżej, bez using Methods; trzeba by wywoływać metodę Methods.KomendyKonsoli.PowiedzCzesc();
+using Methods;
+
+// Mozna także zadeklarować bibliotekę razem z klasą:
+// using static Methods.KomendyKonsoli;
+// korzystając dodatkowo z parametru static, pozwoli to na "importowanie" metod do głównego pliku, ale może powodować komplikacje 
+// w postaci chociażby duplikacji nazw metod pochodzących z różnych bibliotek, zatem nie zaleca się tego używać.
+
+Console.Write("Jak masz na imię: ");
+string imie = Console.ReadLine();
+
+KomendyKonsoli.PowiedzCzesc(imie);
+
+// DRY - Don't Repeat Yourself
+// Zasada polegająca na unikaniu powtarzania partii kodu, przez wrzucanie go w metody i odwoływanie się do nich
+
+// SOLID - zestawa pięciu zasad
+// S - SRP - Single Responsibility Principle
+// Metoda powinna odpowiadać za jeden jedyny spójny logicznie fragment kodu (wykonywać jedną logicznie spójną czynność)
+// Tworzenie metod to nie tylko unikanie powtarzania, warto fragmenty kodu spójne logicznie otaczać metodą ponieważ poprawia to
+// czytelność kodu, pozwalając jednocześnie uniknąć użwania zbędnch komentarzy
+
+Console.WriteLine("Tu znajduje się główna część programu");
+
+KomendyKonsoli.Zegnaj(imie);
