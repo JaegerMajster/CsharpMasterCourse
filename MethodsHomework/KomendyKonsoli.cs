d9cd74cd3a211dc090e06b2dae3783abb92ae3d6
Mariusz Jaguścik <jaguscikm@gmail.com>
2024-07-13 13:52:07 +0200

    Wykonanie zadania domowego nr 10
    Wykonanie zadania domowego dotyczącego wykorzystania metod

---
+++ MethodsHomework/KomendyKonsoli.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsHomework
+{
+    public static class KomendyKonsoli
+    {
+        //1. Stwórz metodę powitalną
+        public static void Powitanie()
+        {
+            Console.WriteLine("Witaj w mojej aplikacji dotyczącej użycia metod w C#!");
+            Console.WriteLine("-----------------------------------------------------");
+        }
+        
+        //2.Stwórz metodę pytającą o imię użytkownika
+        public static string PobierzDane()
+        {
+            Console.Write("Podaj proszę swoje imię: ");
+            string wynik=Console.ReadLine();
+
+            return wynik;
+        }
+
+        //3. Stwórz metodę wypisującą w konsoli: "Witaj <imie>""
+        public static void Komunikat(string imie)
+        {
+            Console.WriteLine($"Witaj {imie}");
+        }
+        public static void Podziekowanie()
+        {
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("Dziękuję za skorzystanie z mojej aplikacji");
+        }
+    }
+}
---
+++ MethodsHomework/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using MethodsHomework;
+
+//4.Wywołaj metodę powitalną
+KomendyKonsoli.Powitanie();
+
+//5. Wywołaj metodę pytającą
+string pierwszeImie = KomendyKonsoli.PobierzDane();
+
+//6. Wywołaj metodą wypisującą
+KomendyKonsoli.Komunikat(pierwszeImie);
+
+//7. Podziękowanie
+KomendyKonsoli.Podziekowanie();
